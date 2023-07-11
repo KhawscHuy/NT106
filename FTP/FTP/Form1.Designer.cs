e1ac2421ad60c2f76d5939a64fb8fb8867894cd8
yuhchak <126562658+KhawscHuy@users.noreply.github.com>
2023-07-11 12:05:02 +0700

    lab6

---
+++ FTP/FTP/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace FTP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonConnect = new System.Windows.Forms.Button();
+            this.textBoxServerIP = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxUsername = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.buttonDownload = new System.Windows.Forms.Button();
+            this.buttonView = new System.Windows.Forms.Button();
+            this.buttonUpload = new System.Windows.Forms.Button();
+            this.list = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // buttonConnect
+            // 
+            this.buttonConnect.Location = new System.Drawing.Point(454, 11);
+            this.buttonConnect.Name = "buttonConnect";
+            this.buttonConnect.Size = new System.Drawing.Size(111, 78);
+            this.buttonConnect.TabIndex = 0;
+            this.buttonConnect.Text = "Connect";
+            this.buttonConnect.UseVisualStyleBackColor = true;
+            this.buttonConnect.Click += new System.EventHandler(this.buttonConnect_Click);
+            // 
+            // textBoxServerIP
+            // 
+            this.textBoxServerIP.Location = new System.Drawing.Point(99, 11);
+            this.textBoxServerIP.Name = "textBoxServerIP";
+            this.textBoxServerIP.Size = new System.Drawing.Size(340, 22);
+            this.textBoxServerIP.TabIndex = 1;
+            this.textBoxServerIP.TextChanged += new System.EventHandler(this.textBoxServerIP_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Server: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(20, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Username:";
+            // 
+            // textBoxUsername
+            // 
+            this.textBoxUsername.Location = new System.Drawing.Point(99, 39);
+            this.textBoxUsername.Name = "textBoxUsername";
+            this.textBoxUsername.Size = new System.Drawing.Size(340, 22);
+            this.textBoxUsername.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 73);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Password:";
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.Location = new System.Drawing.Point(99, 67);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.Size = new System.Drawing.Size(340, 22);
+            this.textBoxPassword.TabIndex = 5;
+            this.textBoxPassword.UseSystemPasswordChar = true;
+            // 
+            // buttonDownload
+            // 
+            this.buttonDownload.Location = new System.Drawing.Point(233, 119);
+            this.buttonDownload.Name = "buttonDownload";
+            this.buttonDownload.Size = new System.Drawing.Size(113, 30);
+            this.buttonDownload.TabIndex = 8;
+            this.buttonDownload.Text = "Download";
+            this.buttonDownload.UseVisualStyleBackColor = true;
+            this.buttonDownload.Click += new System.EventHandler(this.buttonDownload_Click);
+            // 
+            // buttonView
+            // 
+            this.buttonView.Location = new System.Drawing.Point(377, 119);
+            this.buttonView.Name = "buttonView";
+            this.buttonView.Size = new System.Drawing.Size(113, 30);
+            this.buttonView.TabIndex = 12;
+            this.buttonView.Text = "View";
+            this.buttonView.UseVisualStyleBackColor = true;
+            this.buttonView.Click += new System.EventHandler(this.buttonView_Click);
+            // 
+            // buttonUpload
+            // 
+            this.buttonUpload.Location = new System.Drawing.Point(82, 119);
+            this.buttonUpload.Name = "buttonUpload";
+            this.buttonUpload.Size = new System.Drawing.Size(113, 30);
+            this.buttonUpload.TabIndex = 13;
+            this.buttonUpload.Text = "Upload";
+            this.buttonUpload.UseVisualStyleBackColor = true;
+            this.buttonUpload.Click += new System.EventHandler(this.buttonUpload_Click);
+            // 
+            // list
+            // 
+            this.list.HideSelection = false;
+            this.list.Location = new System.Drawing.Point(12, 155);
+            this.list.Name = "list";
+            this.list.Size = new System.Drawing.Size(553, 273);
+            this.list.TabIndex = 15;
+            this.list.UseCompatibleStateImageBehavior = false;
+            this.list.View = System.Windows.Forms.View.List;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(582, 440);
+            this.Controls.Add(this.list);
+            this.Controls.Add(this.buttonUpload);
+            this.Controls.Add(this.buttonView);
+            this.Controls.Add(this.buttonDownload);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxPassword);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxUsername);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxServerIP);
+            this.Controls.Add(this.buttonConnect);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonConnect;
+        private System.Windows.Forms.TextBox textBoxServerIP;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxUsername;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.Button buttonDownload;
+        private System.Windows.Forms.Button buttonView;
+        private System.Windows.Forms.Button buttonUpload;
+        private System.Windows.Forms.ListView list;
+    }
+}
+
---
+++ FTP/FTP/Form1.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FTP
+{
+    public partial class Form1 : Form
+    {
+        private string ftpServerUrl;
+        private string ftpUsername ;
+        private string ftpPassword ;
+        private bool isConnected = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            buttonDownload.Enabled = false;
+            buttonUpload.Enabled = false;
+            buttonView.Enabled = false;
+        }
+
+        private void buttonView_Click(object sender, EventArgs e)
+        {
+            if (!isConnected)
+            {
+                MessageBox.Show("Please connect to the FTP server first.");
+                return;
+            }
+
+            list.Items.Clear();
+
+            try
+            {
+                FtpWebRequest request = (FtpWebRequest)WebRequest.Create(ftpServerUrl);
+                request.Method = WebRequestMethods.Ftp.ListDirectory;
+
+                request.Credentials = new NetworkCredential(ftpUsername, ftpPassword);
+
+                using (FtpWebResponse response = (FtpWebResponse)request.GetResponse())
+                {
+                    using (StreamReader reader = new StreamReader(response.GetResponseStream()))
+                    {
+                        string line = reader.ReadLine();
+                        while (!string.IsNullOrEmpty(line))
+                        {
+                            list.Items.Add(line);
+                            line = reader.ReadLine();
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void buttonConnect_Click(object sender, EventArgs e)
+        {
+            if (buttonConnect.Text == "Connect")
+            {
+                ftpServerUrl = textBoxServerIP.Text;
+                ftpUsername = textBoxUsername.Text;
+                ftpPassword = textBoxPassword.Text;
+                try
+                {
+                    buttonDownload.Enabled = true;
+                    buttonUpload.Enabled = true;
+                    buttonView.Enabled = true;
+                    textBoxServerIP.ReadOnly = true;
+                    textBoxUsername.ReadOnly = true;
+                    textBoxPassword.ReadOnly = true;
+                    buttonConnect.Text = "Disconnect";
+                    FtpWebRequest request = (FtpWebRequest)WebRequest.Create(ftpServerUrl);
+                    request.Method = WebRequestMethods.Ftp.ListDirectory;
+                    request.Credentials = new NetworkCredential(ftpUsername, ftpPassword);
+                    using (FtpWebResponse response = (FtpWebResponse)request.GetResponse())
+                    {
+                        isConnected = true;
+                        MessageBox.Show("Connected to FTP server successfully.");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message);
+                }
+            }
+            else
+            {
+                isConnected = false;
+                buttonDownload.Enabled = false;
+                buttonUpload.Enabled = false;
+                buttonView.Enabled = false;
+                textBoxServerIP.ReadOnly = false;
+                textBoxUsername.ReadOnly = false;
+                textBoxPassword.ReadOnly = false;
+                buttonConnect.Text = "Connect";
+                MessageBox.Show("Disconnected to FTP server successfully.");
+            }
+        }
+
+        private void textBoxServerIP_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonUpload_Click(object sender, EventArgs e)
+        {
+            if (!isConnected)
+            {
+                MessageBox.Show("Please connect to the FTP server first.");
+                return;
+            }
+
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string selectedFilePath = openFileDialog.FileName;
+                string selectedFileName = Path.GetFileName(selectedFilePath);
+
+                try
+                {
+                    FtpWebRequest request = (FtpWebRequest)WebRequest.Create(ftpServerUrl + "/" + selectedFileName);
+                    request.Method = WebRequestMethods.Ftp.UploadFile;
+
+                    request.Credentials = new NetworkCredential(ftpUsername, ftpPassword);
+
+                    using (FileStream fileStream = File.OpenRead(selectedFilePath))
+                    {
+                        using (Stream requestStream = request.GetRequestStream())
+                        {
+                            fileStream.CopyTo(requestStream);
+                        }
+                    }
+
+                    MessageBox.Show("File uploaded successfully.");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message);
+                }
+            }
+        }
+
+        private void buttonDownload_Click(object sender, EventArgs e)
+        {
+            if (!isConnected)
+            {
+                MessageBox.Show("Please connect to the FTP server first.");
+                return;
+            }
+
+            if (list.SelectedItems.Count == 0)
+            {
+                MessageBox.Show("Please select a file.");
+                return;
+            }
+
+            string selectedFile = list.SelectedItems[0].Text;
+
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.FileName = selectedFile;
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string localFilePath = saveFileDialog.FileName;
+
+                try
+                {
+                    FtpWebRequest request = (FtpWebRequest)WebRequest.Create(ftpServerUrl + "/" + selectedFile);
+                    request.Method = WebRequestMethods.Ftp.DownloadFile;
+
+                    request.Credentials = new NetworkCredential(ftpUsername, ftpPassword);
+
+                    using (FtpWebResponse response = (FtpWebResponse)request.GetResponse())
+                    {
+                        using (Stream responseStream = response.GetResponseStream())
+                        {
+                            using (FileStream fileStream = new FileStream(localFilePath, FileMode.Create))
+                            {
+                                responseStream.CopyTo(fileStream);
+                            }
+                        }
+                    }
+
+                    MessageBox.Show("File downloaded successfully.");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message);
+                }
+            }
+        }
+    }
+}
